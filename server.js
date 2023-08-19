1980172db4b821d253d48a87652c5f66c5169dc6
SARATH.S <sarathsivan099@gmail.com>
2023-08-19 10:52:47 +0530

    update --

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('join', (name) => {
+    console.log(`User ${name} joined`);
+    socket.broadcast.emit('userJoined', name);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
